2fdea06abba5855458fed3daf1451e7d850be621
f-wntp27 <wntp27@gmail.com>
2022-01-22 23:07:31 +0700

    close #8 - implement entity PurchaseOrder

--- backend/entity/purchase_order.go
+++ backend/entity/purchase_order.go
@@ -1,8 +1,32 @@
 package entity
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type PaymentMethod struct {
 	gorm.Model
 	MethodName string
+
+	Orders []PurchaseOrder `gorm:"foreignKey:PaymentMethodID"`
+}
+
+type PurchaseOrder struct {
+	gorm.Model
+
+	// MemberID	*uint
+	// Member	Member	`gorm:"references:ID"`
+
+	// PromotionID	*uint
+	// Promotion	Promotion	`gorm:"references:ID"`
+
+	PaymentMethodID *uint
+	PaymentMethod   PaymentMethod `gorm:"references:ID"`
+
+	DeliveryAddress string
+	OrderTime       time.Time
+	OrderDiscount   float64
+	OrderTotalPrice float64
 }
