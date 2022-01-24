2e848a96e41c3fec5aa3f49218e59556302be8f0
f-wntp27 <wntp27@gmail.com>
2022-01-25 01:01:15 +0700

    close #94 - ทำ interface SignIn ใน models

---
+++ frontend/src/models/IUser.ts
@@ -0,0 +1,58 @@
+export interface UserRolesInterface {
+    ID: number;
+    RoleName: string;
+}
+
+export interface UserDetailsInterface {
+    ID: number;
+
+    PrefixID: number;
+    Prefix: PrefixesInterface;
+
+    FirstName: string;
+    LastName: string;
+    PersonalID: string;
+    PhoneNumber: string;
+    Address: string;
+
+    GenderID: number;
+    Gender: GendersInterface;
+}
+
+export interface GendersInterface {
+    ID: number;
+    GenderName: string;
+}
+
+export interface PrefixesInterface {
+    ID: number;
+    PrefixName: string;
+}
+
+export interface MembersInterface {
+    ID: number;
+    
+    UserDetailID: number;
+    UserDetail: UserDetailsInterface;
+
+    // Orders: PurchaseOrdersInterface[];
+}
+
+export interface EmployeesInterface {
+    ID: number;
+
+    UserDetailID: number;
+    UserDetail: UserDetailsInterface;
+
+    PositionID: number;
+    Position: EmployeePositionsInterface;
+
+    SuperiorID: number;
+    Superior: EmployeesInterface;
+}
+
+export interface EmployeePositionsInterface {
+    ID: number;
+    PositionName: string;
+    Salary: number;
+}
