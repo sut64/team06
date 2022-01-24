cd2bcdcd167be77f92dcc655d5b77903e706ea22
fonthap <fonthap.17@gmail.com>
2022-01-24 23:07:34 +0700

    close #35 - Create entity Productstock

--- backend/entity/product_stock.go
+++ backend/entity/product_stock.go
@@ -2,10 +2,29 @@ package entity
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
+
+type Productstock struct {
+	gorm.Model
+	Amount_remain   uint
+	Update_datetime time.Time
+	Detail          string 
+
+	ProductID *uint
+	Product   Product
+
+	ShelfstoreID *uint
+	Shelfstore   Shelfstore
+
+	EmployeeID *uint
+	Employee   Employee
+}
 
 
 type Typeproduct struct {
 	gorm.Model
 	Name string
+	
+	Product []Product
 }
