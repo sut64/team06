fd5f8743829c2fa6718bfc92f79b61c7def8b9bc
Sakeezt <sgame115500@gmail.com>
2022-01-24 22:50:09 +0700

    close #15 - ทำ entity PromotionType

--- backend/entity/manage_promotion.go
+++ backend/entity/manage_promotion.go
@@ -1,1 +1,13 @@
 package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+type PromotionType struct {
+	gorm.Model
+
+	Type string
+
+	//ManagePromotions []ManagePromotion `gorm:"foreignKey:PromotionTypeID"`
+}
