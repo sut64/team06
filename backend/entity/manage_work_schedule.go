fd8aad422b4f35f532544b1bc70477a17ebeeb24
PuwanatAsphere <puwanat.t@asphere-global.com>
2022-01-27 04:20:06 +0700

    test

--- backend/entity/manage_work_schedule.go
+++ backend/entity/manage_work_schedule.go
@@ -1,33 +1,70 @@
 package entity
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
+
+type ManageWorkTime struct {
+	gorm.Model
+	NameSchedule string
+	WorkingDate  time.Time
+	TimeTotal    uint
+
+	ManagerID *uint
+	Manager   Manager
+
+	EmployeeID *uint
+	Employee   Employee
+
+	DayID *uint
+	Day   Day
+
+	WeeklyID *uint
+	Weekly   Weekly
+
+	MonthID *uint
+	Month   Month
+
+	WorkingTimeID *uint
+	WorkingTime   WorkingTime
+
+	ManageSalary []ManageSalary `gorm:"foreignkey:ManageWorkTimeID"`
+}
 
 type Day struct {
 	gorm.Model
 	DayOfWeek string
 
-	// ManageWorkTime []ManageWorkTime `gorm:"foreignKey:DayID"`
+	ManageWorkTime []ManageWorkTime `gorm:"foreignKey:DayID"`
 }
 
 type Month struct {
 	gorm.Model
 	MonthOfYear string
 
-	// ManageWorkTime []ManageWorkTime `gorm:"foreignKey:MonthID"`
+	ManageWorkTime []ManageWorkTime `gorm:"foreignKey:MonthID"`
 }
 
 type Weekly struct {
 	gorm.Model
 	WeekAt uint
 
-	// ManageWorkTime []ManageWorkTime `gorm:"foreignKey:WeeklyID"`
+	ManageWorkTime []ManageWorkTime `gorm:"foreignKey:WeeklyID"`
 }
 
 type WorkingTime struct {
 	gorm.Model
 	TimeToTime string
 
-	// ManageWorkTime []ManageWorkTime `gorm:"foreignKey:WorkingTimeID"`
+	ManageWorkTime []ManageWorkTime `gorm:"foreignKey:WorkingTimeID"`
+}
+
+type Manager struct {
+	gorm.Model
+	Name     string
+	Code     string
+	Password string
+	Employee []Employee `gorm:"foreignKey:ManagerID"`
 }
